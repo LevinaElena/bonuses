0b212f2f259f1faa01111b0def37ba821e0240ca
LevinaElena <e.a.kolupanovich@gmail.com>
2022-11-01 21:59:45 +0200

    final-end

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -16,9 +16,9 @@ const fetcher = url => axios.get(url).then(res => res.data)
 
 const FaqBlock:NextPage<questionAnswerProps> = ({qa}) => {
 
-    const { data: casino } = useSWR(`https://bonuses-grpem4oxt-levinaelena.vercel.app/api/topcasinos/`,fetcher);
-    const { data: topBonuses } = useSWR('https://bonuses-grpem4oxt-levinaelena.vercel.app/api/topbonuses',fetcher);
-    const { data: games } = useSWR('https://bonuses-grpem4oxt-levinaelena.vercel.app/api/topgames',fetcher);
+    const { data: casino } = useSWR(`https://bonuses-levinaelena.vercel.app/api/topcasinos/`,fetcher);
+    const { data: topBonuses } = useSWR('https://bonuses-levinaelena.vercel.app/api/topbonuses',fetcher);
+    const { data: games } = useSWR('https://bonuses-levinaelena.vercel.app/api/topgames',fetcher);
 
     return (
         <div className={styles.container_faq}>
@@ -37,7 +37,7 @@ const FaqBlock:NextPage<questionAnswerProps> = ({qa}) => {
 
 export const getStaticProps:GetStaticProps = async () => {
     try {
-        const response = await fetch(`https://bonuses-grpem4oxt-levinaelena.vercel.app/api/faq/`);
+        const response = await fetch(`https://bonuses-levinaelena.vercel.app/api/faq/`);
         const data = await response.json();
         if (!data) {
             return { notFound: true,}
